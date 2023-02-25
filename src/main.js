434f929572186ff886475f0cb7dd0d7ea6057c93
jerome <javarome@gmail.com>
2023-02-25 17:05:35 +0100

    Update project

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import {ArnServerClientFactory} from "@arianeeprivate/arn-client"
 import "@arianeeprivate/arn-components"
 
 const clientFactory = new ArnServerClientFactory()
-const projectUrl = "https://arn-server-ysl-v2-staging.arianee.com/yslbeauty"
+const projectUrl = "https://arn-server-ysl-dev-quvnqrx5rq-ew.a.run.app/testProject"
 const config = await clientFactory.createConfigFromUrl(projectUrl)
 const arnClient = clientFactory.create(config)
 window.arnClient = arnClient
