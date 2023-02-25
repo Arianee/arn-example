1cb524526bcef9c57ce273d6ffd4f5d246adc54d
jerome <javarome@gmail.com>
2023-02-25 17:18:50 +0100

    lit example

---
+++ src/my-element.js
@@ -0,0 +1,37 @@
+import { LitElement, html } from 'lit'
+
+/**
+ * An example element.
+ *
+ * @slot - This element has a slot
+ * @csspart button - The button
+ */
+export class MyElement extends LitElement {
+  static get properties() {
+    return {
+      /**
+       * Copy for the read the docs hint.
+       */
+      docsHint: { type: String },
+
+      /**
+       * The number of times the button has been clicked.
+       */
+      count: { type: Number },
+    }
+  }
+
+  constructor() {
+    super()
+    this.docsHint = 'Click on the Vite and Lit logos to learn more'
+    this.count = 0
+  }
+
+  render() {
+    return html`
+      <slot></slot>
+    `
+  }
+}
+
+window.customElements.define('my-element', MyElement)
--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,14 @@
 import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
-import {NodeGlobalsPolyfillPlugin} from "@esbuild-plugins/node-globals-polyfill"
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
-  // See https://github.com/WalletConnect/walletconnect-monorepo/issues/734
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: "globalThis"
-      },
-      // Enable esbuild polyfill plugins
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true
-        })
-      ]
-    }
-  }
+  build: {
+    lib: {
+      entry: 'src/my-element.js',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: /^lit/,
+    },
+  },
 })
