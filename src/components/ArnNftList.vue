6ea4d75789b85aa352aab9fbb162a865d1ada9ec
jerome <javarome@gmail.com>
2023-03-02 16:56:07 +0100

    Update with nft.getList()

--- src/components/ArnNftList.vue
+++ src/components/ArnNftList.vue
@@ -1,5 +1,7 @@
 <script setup>
 
+import {ArnConnectionStatus} from "@arianeeprivate/arn-client"
+
 const props = defineProps({
   tag: String
 })
@@ -10,16 +12,27 @@ let foundNFTs;
 const findNFTs = async() => {
   foundNFTs = await arnClient.nft.getList({tags: [props.tag]})
 }
-findNFTs()
+arnClient.auth.currentContext$.subscribe(async (authContext) => {
+  authContext?.status$.subscribe((status) => {
+    if (status?.connectionStatus === ArnConnectionStatus.authenticated) {
+      findNFTs()
+    }
+  });
+});
 </script>
 
 <template>
-  <section>
-    <h2>NFT list by tag</h2>
-    <p>This is a sample usage of the <code>arnClient.nft.getList(tag)</code> API:</p>
-    <ul>
-      <li v-for="nft in foundNFTs?.length" :key="nft.id">{{ nft.id }}</li>
-    </ul>
-  </section>
+  <h2>NFT list by tag</h2>
+  <p>This is a sample usage of the <code>arnClient.nft.getList(tag)</code> API:</p>
+  <arn-if-connected>
+    <div slot="if-true">
+      <ul>
+        <li v-for="nft in foundNFTs?.length" :key="nft.id">{{ nft.id }}</li>
+      </ul>
+    </div>
+    <div slot="if-false">
+      (you can list NFTs only when connected)
+    </div>
+  </arn-if-connected>
 </template>
 
