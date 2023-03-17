48a2d27c44ec4bf62db732759d8686230a458e43
jerome <javarome@gmail.com>
2023-03-17 13:09:27 +0100

    Fix example

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-import {ArnServerClientFactory} from "@arianeeprivate/arn-client"
-import "@arianeeprivate/arn-components";
-import { ConnectExample } from "./ConnectExample"
+import {ArnHttpClientFactory} from "@arianeeprivate/arn-client"
+import "@arianeeprivate/arn-components"
+import {ConnectExample} from "./ConnectExample"
 import {IfConnectedExample} from "./IfConnectedExample.js"
 
-const clientFactory = new ArnServerClientFactory();
-const projectUrl = "https://arn-server-ysl-dev-quvnqrx5rq-ew.a.run.app/testProject"
-const config = await clientFactory.createConfigFromUrl(projectUrl)
-const arnClient = clientFactory.create(config);
-window.arnClient = arnClient
+const clientFactory = new ArnHttpClientFactory()
+clientFactory.createFromUrl("https://arn-server-ysl-dev-quvnqrx5rq-ew.a.run.app/testProject")
+  .then(arnClient => {
+    window.arnClient = arnClient
 
-const connectExample = new ConnectExample('Connect your wallet', 'Disconnect your wallet')
-connectExample.render(document.querySelector("#arn-connect"));
+    const connectExample = new ConnectExample("Connect your wallet", "Disconnect your wallet")
+    connectExample.render(document.querySelector("#arn-connect"))
 
-const ifConnectedExample = new IfConnectedExample('You are connected!', 'You are disconnected.')
-ifConnectedExample.render(document.querySelector("#arn-if-connected"));
+    const ifConnectedExample = new IfConnectedExample("You are connected!", "You are disconnected.")
+    ifConnectedExample.render(document.querySelector("#arn-if-connected"))
+  })
