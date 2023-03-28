744aba857721fbbdccb75840c77b50dd775f6197
jerome <javarome@gmail.com>
2023-03-28 11:42:24 +0200

    Use to latest libs

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,21 @@
-import {ArnServerClientFactory} from "@arianeeprivate/arn-client"
-import "@arianeeprivate/arn-components";
-import { ConnectExample } from "./ConnectExample"
+import {ArnHttpClientFactory} from "@arianeeprivate/arn-client"
+import "@arianeeprivate/arn-components"
+import {ConnectExample} from "./ConnectExample"
 import {IfConnectedExample} from "./IfConnectedExample"
 
-const clientFactory = new ArnServerClientFactory();
-const projectUrl = "https://arn-server-ysl-dev-quvnqrx5rq-ew.a.run.app/testProject"
-const config = await clientFactory.createConfigFromUrl(projectUrl)
-const arnClient = clientFactory.create(config);
-(window as any).arnClient = arnClient
+const clientFactory = new ArnHttpClientFactory()
+clientFactory.createFromUrl("https://arn-server-test-dev.arianee.com/testproject").then(arnClient => {
+  (window as any).arnClient = arnClient
 
-const connectExample = new ConnectExample('Connect your wallet', 'Disconnect your wallet')
-const connectAnchor = document.querySelector("#arn-connect-example")
-if (connectAnchor) {
-  connectExample.render(connectAnchor)
-}
+  const connectExample = new ConnectExample("Connect your wallet", "Disconnect your wallet")
+  const connectAnchor = document.querySelector("#arn-connect-example")
+  if (connectAnchor) {
+    connectExample.render(connectAnchor)
+  }
 
-const ifConnectedExample = new IfConnectedExample('You are connected!', 'You are disconnected.')
-const ifConnectedAnchor = document.querySelector("#arn-if-connected-example")
-if (ifConnectedAnchor) {
-  ifConnectedExample.render(ifConnectedAnchor)
-}
+  const ifConnectedExample = new IfConnectedExample("You are connected!", "You are disconnected.")
+  const ifConnectedAnchor = document.querySelector("#arn-if-connected-example")
+  if (ifConnectedAnchor) {
+    ifConnectedExample.render(ifConnectedAnchor)
+  }
+})
