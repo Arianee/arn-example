ee5c57b7456f0c9e01907a5aff9689345b7a5acc
jerome <javarome@gmail.com>
2023-03-28 11:30:00 +0200

    Updated to latest libs

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
 
-import {ArnServerClientFactory} from "@arianeeprivate/arn-client"
-import "@arianeeprivate/arn-components";
+import {ArnHttpClientFactory} from "@arianeeprivate/arn-client"
+import "@arianeeprivate/arn-components"
 
-const clientFactory = new ArnServerClientFactory();
-const projectUrl = "https://arn-server-ysl-dev-quvnqrx5rq-ew.a.run.app/testProject"
-const config = await clientFactory.createConfigFromUrl(projectUrl)
-const arnClient = clientFactory.create(config);
-window.arnClient = arnClient
+const clientFactory = new ArnHttpClientFactory()
+clientFactory.createFromUrl("https://arn-server-test-dev.arianee.com/testproject").then(arnClient => {
+  window.arnClient = arnClient
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode> <App/> </React.StrictMode>
+  )
+})
