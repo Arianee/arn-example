b08c84aa227099cb73c786ef505035f9b03a1e48
jerome <javarome@gmail.com>
2023-03-28 17:47:51 +0200

    Use public libs

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import "./app.css"
 import App from "./App.svelte"
 
-import {ArnHttpClientFactory} from "@arianeeprivate/arn-client"
-import "@arianeeprivate/arn-components"
+import {ArnHttpClientFactory} from "@arianee/arn-client"
+import "@arianee/arn-components"
 
 const clientFactory = new ArnHttpClientFactory()
 const arnClient = await clientFactory.createFromUrl("https://arn-server-test-dev.arianee.com/testproject")
