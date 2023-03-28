a641892633e277793775bb20d956c1ef7a78af65
jerome <javarome@gmail.com>
2023-03-28 11:24:45 +0200

    Use testproject

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,12 @@ import {platformBrowserDynamic} from "@angular/platform-browser-dynamic"
 
 import {AppModule} from "./app/app.module"
 
-import {ArnServerClientFactory} from "@arianeeprivate/arn-client"
+import {ArnHttpClientFactory} from "@arianeeprivate/arn-client"
 import "@arianeeprivate/arn-components"
 
-const clientFactory = new ArnServerClientFactory()
-const projectUrl = "https://arn-server-ysl-dev-quvnqrx5rq-ew.a.run.app/testProject"
-clientFactory.createConfigFromUrl(projectUrl).then(config => {
-  (window as any).arnClient = clientFactory.create(config)
+const clientFactory = new ArnHttpClientFactory()
+clientFactory.createFromUrl("https://arn-server-test-dev.arianee.com/testproject").then(arnClient => {
+  (window as any).arnClient = arnClient
   platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.error(err))
 })
