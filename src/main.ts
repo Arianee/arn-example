9378c932d48cf113f66bd7928c50ded879b39ee7
jerome <javarome@gmail.com>
2023-03-28 17:43:28 +0200

    Use to public libs

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import {platformBrowserDynamic} from "@angular/platform-browser-dynamic"
 
 import {AppModule} from "./app/app.module"
 
-import {ArnHttpClientFactory} from "@arianeeprivate/arn-client"
-import "@arianeeprivate/arn-components"
+import {ArnHttpClientFactory} from "@arianee/arn-client"
+import "@arianee/arn-components"
 
 const clientFactory = new ArnHttpClientFactory()
 clientFactory.createFromUrl("https://arn-server-test-dev.arianee.com/testproject").then(arnClient => {
