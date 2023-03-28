ea4fc0e0c33fc148eaf6f04d9c2a549d065b196d
jerome <javarome@gmail.com>
2023-03-28 11:32:33 +0200

    Use latest libs

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-import { render } from 'preact'
-import { App } from './app.jsx'
+import {render} from "preact"
+import {App} from "./app.jsx"
 
-import {ArnServerClientFactory} from "@arianeeprivate/arn-client"
-import "@arianeeprivate/arn-components";
+import {ArnHttpClientFactory} from "@arianeeprivate/arn-client"
+import "@arianeeprivate/arn-components"
 
-const clientFactory = new ArnServerClientFactory();
-const projectUrl = "https://arn-server-ysl-dev-quvnqrx5rq-ew.a.run.app/testProject"
-const config = await clientFactory.createConfigFromUrl(projectUrl)
-const arnClient = clientFactory.create(config);
-window.arnClient = arnClient
-
-render(<App />, document.getElementById('app'))
+const clientFactory = new ArnHttpClientFactory()
+const config = clientFactory.createFromUrl("https://arn-server-test-dev.arianee.com/testproject").then(arnClient => {
+  window.arnClient = arnClient
+  render(<App/>, document.getElementById("app"))
+})
