82ecb06b5726a1ecc1759397bb7784542aa39e4f
jerome <javarome@gmail.com>
2023-03-28 17:46:45 +0200

    Use public libs

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import {render} from "preact"
 import {App} from "./app.jsx"
 
-import {ArnHttpClientFactory} from "@arianeeprivate/arn-client"
-import "@arianeeprivate/arn-components"
+import {ArnHttpClientFactory} from "@arianee/arn-client"
+import "@arianee/arn-components"
 
 const clientFactory = new ArnHttpClientFactory()
-const config = clientFactory.createFromUrl("https://arn-server-test-dev.arianee.com/testproject").then(arnClient => {
+clientFactory.createFromUrl("https://arn-server-test-dev.arianee.com/testproject").then(arnClient => {
   window.arnClient = arnClient
   render(<App/>, document.getElementById("app"))
 })
