2aaeef58195560875ec4547aaaa7f7c679d8805e
jerome <javarome@gmail.com>
2023-03-31 14:13:21 +0200

    Update to latest libs

--- src/IfConnectedExample.ts
+++ src/IfConnectedExample.ts
@@ -1,6 +1,29 @@
+import {ArnClient} from "@arianee/arn-client"
+
+declare const arnClient: ArnClient
+
 export class IfConnectedExample {
+  protected walletAddress?: string
+  protected latestAnchor?: Element
 
   constructor(protected msgConnected: string, protected msgDisconnected: string) {
+    this.walletAddress = ""
+    // Listen to status to refresh wallet address
+    arnClient.auth.currentContext$.subscribe(async (authContext) => {
+      authContext?.status$.subscribe((status) => {
+        switch (status?.connectionStatus) {
+          case "authenticated":
+            this.walletAddress = status.address
+            break
+          case "disconnected":
+            this.walletAddress = ""
+            break
+        }
+        if (this.latestAnchor) {
+          this.render(this.latestAnchor)
+        }
+      })
+    })
   }
 
   render(anchor: Element) {
@@ -10,8 +33,9 @@ export class IfConnectedExample {
   <p>This is a sample usage of the <code>&lt;arn-if-connected&gt;</code> ARN component:</p>
   <arn-if-connected>
     <p slot="if-false">${this.msgDisconnected}</p>
-    <p slot="if-true">${this.msgConnected}</p>
+    <p slot="if-true">${this.msgConnected} ${this.walletAddress}</p>
   </arn-if-connected>
 </section>`
+    this.latestAnchor = anchor
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,14 @@ import {IfConnectedExample} from "./IfConnectedExample"
 import {NftListExample} from "./NftListExample"
 
 const clientFactory = new ArnHttpClientFactory()
-clientFactory.createFromUrl("https://arn-server-test-dev.arianee.com/testproject").then(arnClient => {
-  (window as any).arnClient = arnClient
-
+clientFactory.createFromUrl("https://arn-server-test-dev.arianee.com/testproject").then(() => {
   const connectExample = new ConnectExample("Connect your wallet", "Disconnect your wallet")
   const connectAnchor = document.querySelector("#arn-connect-example")
   if (connectAnchor) {
     connectExample.render(connectAnchor)
   }
 
-  const ifConnectedExample = new IfConnectedExample("You are connected!", "You are disconnected.")
+  const ifConnectedExample = new IfConnectedExample("You are connected with wallet", "You are disconnected.")
   const ifConnectedAnchor = document.querySelector("#arn-if-connected-example")
   if (ifConnectedAnchor) {
     ifConnectedExample.render(ifConnectedAnchor)
