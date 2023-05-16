65aca2209f95f1897260b4b12f75f17245565b74
jerome <javarome@gmail.com>
2023-05-16 15:49:22 +0200

    fix(package): update to latest libs

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import IfConnectedExample from "./connect-example/IfConnectedExample.jsx"
 function App() {
   return (
     <div className="App">
-      <h1>ARN example</h1>
+      <h1><img className="logo" src="https://storage.googleapis.com/arn3-static-resources/arn/arn.png" alt="ARN logo" width="50"/> Example</h1>
       <p>This is a React (built by Vite) version of the ARN example.</p>
       <ConnectExample connectMsg='Connect your wallet' disconnectMsg='Disconnect your wallet'/>
       <IfConnectedExample connectedMsg='You are connected using wallet' disconnectedMsg='You are disconnected.'/>
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
+import "./main.css"
 
 import {ArnHttpClientFactory} from "@arianee/arn-client"
 import "@arianee/arn-components"  // Register ARN web components
 
 const clientFactory = new ArnHttpClientFactory()
-clientFactory.createFromUrl("https://arn-server-test-dev.arianee.com/testproject").then(() => {
+clientFactory.createFromUrl("https://arn-server-test-prod.arianee.com/testproject").then(() => {
   ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode> <App/> </React.StrictMode>
   )
