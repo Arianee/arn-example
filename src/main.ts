46de42cfbfd7933ab4f6742e99e3be8e8e6773e5
jerome <javarome@gmail.com>
2023-06-26 15:54:09 +0200

    fix(i18n): proper config

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import {NftListExample} from "./NftListExample"
 import {TranslationExample} from "./TranslationExample"
 
 const clientFactory = new ArnHttpClientFactory()
-clientFactory.createFromUrl("https://arn-server-moncler-staging.arianee.com/arn-staging").then(() => {
+clientFactory.createFromUrl("https://arn-server-test-prod.arianee.com/testproject").then(() => {
   const connectExample = new ConnectExample("Connect your wallet", "Disconnect your wallet")
   const connectAnchor = document.querySelector("#arn-connect-example")
   if (connectAnchor) {
@@ -25,6 +25,9 @@ clientFactory.createFromUrl("https://arn-server-moncler-staging.arianee.com/arn-
   if (nftListAnchor) {
     nftListExample.render()
   }
+
+  const long = 'x'.repeat(4.9*1024*1024)
+  localStorage.setItem('key', long)
 
   const i18nAnchor = document.querySelector("#translation-example")!
   const translationExample = new TranslationExample(i18nAnchor)
