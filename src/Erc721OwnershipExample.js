f33e21f9f6f196a719b01d53b2c97afe9d90e5f1
jerome <javarome@gmail.com>
2023-07-11 19:35:47 +0200

    feat(condition): ERC721 ownership condition check

---
+++ src/Erc721OwnershipExample.js
@@ -0,0 +1,69 @@
+import {HasERC721Condition} from "@arianee/arn-types"
+
+export class Erc721OwnershipExample {
+
+  constructor(anchor) {
+    this.anchor = anchor;
+    this.contractAddress = ''
+    this.message = ''
+    // Listen to auth status to refresh wallet address
+    arnClient.auth.currentContext$.subscribe(async (authContext) => {
+      authContext?.status$.subscribe((status) => {
+        switch (status?.connectionStatus) {
+          case 'authenticated':
+            this.checkOwnership()
+            break
+          case 'disconnected':
+            break
+        }
+        if (this.latestAnchor) {
+          this.render(this.latestAnchor)
+        }
+      })
+    })
+  }
+
+  async checkOwnership() {
+    if (this.contractAddress) {
+      const minBalance = 1  // At least one NFT
+      const nftOwnershipCondition = new HasERC721Condition({
+        chainId: 1, // Ethereum
+        contractAddress: this.contractAddress,
+        minBalance
+      })
+      try {
+        const result = await arnClient.condition.execute({and: [nftOwnershipCondition]})
+        if (result.success) {
+          this.message = `The user owns at least ${minBalance} NFT of the required collection`
+        } else {
+          this.message = `The user doesn't own enough NFT of the required collection`
+        }
+      } catch (e) {
+        this.message = 'Error: ' + e.message
+      }
+    } else {
+      this.message = 'Collection address expected'
+    }
+    this.render()
+  }
+
+  render() {
+    this.anchor.innerHTML = `
+<section>
+  <h2>ERC721 ownership</h2>
+  <p>This is a sample usage of the ERC721 ownership:</p>
+    <label>Collection address: </label>
+  <p>${this.message}</p>
+</section>`
+    const input = this.input = document.createElement("input")
+    input.value = this.contractAddress
+    input.type = "search"
+    input.onchange = () => {
+      this.contractAddress = input.value
+      this.checkOwnership()
+    }
+    this.anchor.querySelector("label").append(input)
+
+    this.latestAnchor = this.anchor
+  }
+}
--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import "@arianee/arn-components"
 import {ConnectExample} from "./ConnectExample"
 import {IfConnectedExample} from "./IfConnectedExample.js"
 import {NftListExample} from "./NftListExample.js"
+import {Erc721OwnershipExample} from "./Erc721OwnershipExample.js"
 
 const clientFactory = new ArnHttpClientFactory()
 clientFactory.createFromUrl("https://arn-server-test-prod.arianee.com/testproject")
-  .then(() => {
+  .then(async (_arnClient) => {
     const connectExample = new ConnectExample("Connect your wallet", "Disconnect your wallet")
     connectExample.render(document.querySelector("#arn-connect"))
 
@@ -15,4 +16,7 @@ clientFactory.createFromUrl("https://arn-server-test-prod.arianee.com/testprojec
 
     const nftListExample = new NftListExample(document.querySelector("#arn-nft-list"), "myTag")
     nftListExample.render()
+
+    const erc721 = new Erc721OwnershipExample(document.querySelector("#arn-erc721"))
+    erc721.render()
   })
