7875960ebc406d385971dcd6c78fc16b0ba0feab
jerome <javarome@gmail.com>
2023-07-19 11:56:20 +0200

    fix(libs): upgrade to latest versions

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,10 @@ import App from "./App.jsx"
 import "./main.css"
 
 import {ArnHttpClientFactory} from "@arianee/arn-client"
-import "@arianee/arn-components"  // Register ARN web components
+import "@arianee/arn-components" // Register ARN web components
 
 const clientFactory = new ArnHttpClientFactory()
-clientFactory.createFromUrl("https://arn-server-test-prod.arianee.com/testproject").then(() => {
+clientFactory.createFromUrl("https://arn-server-test-prod.arianee.com/testproject").then((_arnClient) => {
   ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode> <App/> </React.StrictMode>
   )
