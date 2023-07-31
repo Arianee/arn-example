8a9f4385a0d7e9d69259785704daf258b3755597
jerome <javarome@gmail.com>
2023-07-31 12:09:43 +0200

    fix(auth): Sign callback example

--- src/main.js
+++ src/main.js
@@ -6,8 +6,10 @@ import {ArnHttpClientFactory} from "@arianee/arn-client"
 import "@arianee/arn-components"  // Register ARN web components
 
 const clientFactory = new ArnHttpClientFactory()
-clientFactory.createFromUrl("https://arn-server-test-prod.arianee.com/testproject").then(arn => {
+clientFactory.createConfigFromUrl("https://arn-server-test-prod.arianee.com/testproject").then(arnConfig => {
+  arnConfig.auth.signCallback = () => window.confirm('Ok to sign?')
+  const arnClient = clientFactory.create(arnConfig)
   const app = createApp(App)
-  app.provide('arn', arn)
+  app.provide('arn', arnClient)
   app.mount("#app")
 })
