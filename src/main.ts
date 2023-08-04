dddaf3a9f639ac0aa07b5d90db16316b677f9aad
jerome <javarome@gmail.com>
2023-08-04 17:50:06 +0200

    refactor(main): use latest API

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
-
 import { ArnHttpClientFactory } from '@arianee/arn-client';
 import '@arianee/arn-components';
-import { ArnLogLevel } from '@arianee/arn-types';  // Register ARN web components
 
 const clientFactory = new ArnHttpClientFactory();
 clientFactory.createConfigFromUrl('https://arn-server-test-prod.arianee.com/testproject').then(arnConfig => {
-  arnConfig.auth.signCallback = async () => window.confirm('Ok to sign?');
-  Object.assign(arnConfig.log, {
-    logLevels: [
-      ArnLogLevel.warn,
-      ArnLogLevel.error,
-      ArnLogLevel.info,
-      ArnLogLevel.debug
-    ]
-  });
+  arnConfig.auth.beforeSign = async () => window.confirm('Ok to sign?');
   clientFactory.create(arnConfig);
   platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.error(err));
