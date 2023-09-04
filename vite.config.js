4ce2ebba89d036ffb8132ba8b1902c979d04bf18
jerome <javarome@gmail.com>
2023-09-04 12:05:45 +0200

    fix(libs): workaround name collision

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 import {defineConfig} from "vite"
+import {readFile} from 'node:fs/promises'; // required import statement
 
 export default defineConfig({
   // ...other config settings
@@ -12,6 +13,16 @@ export default defineConfig({
       },
       // Enable esbuild polyfill plugins
       plugins: [
+        {                                                                    //
+          name: 'raffy-fix',                                                 //
+          setup(build) {                                                     // plugin
+            build.onLoad({ filter: /ens-normalize/ }, async args => {        // that
+              let contents = await readFile(args.path, {encoding: 'utf8'});  // renames
+              contents = contents.replaceAll('process(', 'raffy_process(');  // process()
+              return {contents};                                             // to
+            });                                                              // raffy_process()
+          },                                                                 //
+        },
         NodeGlobalsPolyfillPlugin({
           buffer: true
         })
